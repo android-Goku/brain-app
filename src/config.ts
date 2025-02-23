c5fab16ed7cda1ae26f2e1fc9d304cfcf399c873
Budhdeo Kumar <33325543+android-Goku@users.noreply.github.com>
2025-02-23 20:26:23 +0530

    Delete src/config.ts
    Delete this because it contains passwords

--- src/config.ts
+++
@@ -1,3 +0,0 @@
-export const DB_STRING = "mongodb+srv://budhdeo:B%40kumar1@cluster0.71oln.mongodb.net/brain-app";
-export const JWT_USER_SECRET = "USER123";
-export const JWT_ADMIN_SECRET = "ADMIN";
